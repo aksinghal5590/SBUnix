31fa1299588d13c8532a6c902c87b83ac85361cd
Vaibhav Rustagi <vrustagi@cs.stonybrook.edu>
2017-09-19 02:27:48 -0400

    shift update

--- sys/keyboard.c
+++ sys/keyboard.c
@@ -1,11 +1,21 @@
+#include <sys/string.h>
 #include <sys/interrupt.h>
 #include <sys/kprintf.h>
+
 
 extern uint8_t scancodes[256][2];
 
 void initKeyboard() {
 
 	initScanCodeTable();
+}
+
+void printKeyboardChar(uint8_t printChar)
+{
+	char s[2] = {};
+	s[0] = (char)printChar;
+	s[1] ='\0';
+	kprintf("Input character: %s\n", s);
 }
 
 uint8_t special_char = 0;
@@ -32,6 +42,100 @@ void keyboardImpl() {
 					c -= 32;
 					char s[] = {(char)c, '\0'};
 					kprintf("Input character: %s\n", s);
+				}
+				else {
+					uint8_t printChar = 0;
+					switch((char)c)
+					{
+						case '`':
+							printChar = 126;
+							printKeyboardChar(printChar);
+							break;
+						case '1':
+							printChar = 33;
+							printKeyboardChar(printChar);
+							break;
+						case '2':
+							printChar = 64;
+							printKeyboardChar(printChar);
+							break;
+						case '3':	
+							printChar = 35;
+							printKeyboardChar(printChar);
+							break;
+						case '4':
+							printChar = 36;
+							printKeyboardChar(printChar);
+							break;
+						case '5':
+							printChar = 37;
+							printKeyboardChar(printChar);
+							break;
+						case '6':
+							printChar = 94;
+							printKeyboardChar(printChar);
+							break;
+						case '7':
+							printChar = 38;
+							printKeyboardChar(printChar);
+							break;
+						case '8':
+							printChar = 42;
+							printKeyboardChar(printChar);
+							break;
+						case '9':
+							printChar = 40;
+							printKeyboardChar(printChar);
+							break;
+						case '0':
+							printChar = 41;
+							printKeyboardChar(printChar);
+							break;
+						case '-':
+							printChar = 95;
+							printKeyboardChar(printChar);
+							break;
+						case '=':
+							printChar = 43;
+							printKeyboardChar(printChar);
+							break;
+						case '[':
+							printChar = 123;
+							printKeyboardChar(printChar);
+							break;
+						case ']':
+							printChar = 125;
+							printKeyboardChar(printChar);
+							break;
+						case '\\':
+							printChar = 124;
+							printKeyboardChar(printChar);
+							break;
+						case ';':
+							printChar = 58;
+							printKeyboardChar(printChar);
+							break;
+						case '\'':
+							printChar = 34;
+							printKeyboardChar(printChar);
+							break;
+						case ',':
+							printChar = 60;
+							printKeyboardChar(printChar);
+							break;
+						case '.':
+							printChar = 62;
+							printKeyboardChar(printChar);
+							break;
+						case '/':
+							printChar = 63;
+							printKeyboardChar(printChar);
+							break;
+						default:
+							printChar = c;
+							printKeyboardChar(printChar);
+							break;
+					}
 				}
 			} else if(special_char == 129) {
 				if(c >= 97 && c <= 122) {
