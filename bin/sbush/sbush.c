40766a43ee8cd7c822c13052602a6c2b2ea432f2
ashuyadav93 <ashish.yadav.1@stonybrook.edu>
2017-12-05 00:32:40 -0500

    sbiush changes for fork

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -5,6 +5,10 @@ int main() {
   write(1, "OS by Akshat, Ashish and Vaibhav, version1", 20);
   write(1, "OS by Akshat, Ashish and Vaibhav, version2", 20);
   write(1, "OS by Akshat, Ashish and Vaibhav, version3", 20);
+  int f = fork();
+  if(f==0) {
+     write(1, f, 20);
+  }
   /*char buf[1024];
   read(0, buf, 10);
   write(1, buf, 10);
